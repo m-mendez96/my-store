01944f8e0e2b331819eb3593d7f399d70384d61c
Miguel Mendez <miguel.mendez@novateva.com>
2022-02-17 23:03:23 -0400

    Asyn, try catch

--- services/products.js
+++ services/products.js
@@ -22,7 +22,7 @@ class ProductsService {
         return this.products;
     }
 
-    create(body){
+    async create(body){
         const newProduct = {
             id: faker.datatype.uuid(),
             name: faker.commerce.productName(),
@@ -33,15 +33,15 @@ class ProductsService {
         return newProduct;
     }
 
-    find(){
+    async find(){
         return this.products;
     }
 
-    findOne(id){
+    async findOne(id){
         return this.products.find(item => item.id == id);
     }
 
-    update(id, changes){
+    async update(id, changes){
         const index = this.products.findIndex(item => item.id === id);
         if (index === -1){
             throw new Error("product not found");
@@ -54,7 +54,7 @@ class ProductsService {
         return this.products[index];
     }
 
-    delete(id){
+    async delete(id){
         const index = this.products.findIndex(item => item.id === id);
         if (index === -1){
             throw new Error("product not found");
