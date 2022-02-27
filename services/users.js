a255eec0e87d683535c8700d557dd2d631d65e29
Miguel Mendez <miguel.mendez@novateva.com>
2022-02-27 17:59:55 -0400

    Create model user

--- services/users.js
+++ services/users.js
@@ -1,7 +1,7 @@
 const { faker } = require('@faker-js/faker');
 const boom = require("@hapi/boom");
 
-const getConnection = require('../libs/postgres');
+const { models } = require('../libs/sequelize');
 
 class UsersSerivce {
     
@@ -35,9 +35,8 @@ class UsersSerivce {
     }
 
     async find() {
-        const client = await getConnection();
-        const rta = await client.query('SELECT * FROM tasks');
-        return rta.rows;
+        const rta = await models.User.findAll();
+        return rta;
     }
 
     async findOne(id){
