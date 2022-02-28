40ba1b7438f86a7b8063f3e1a1810274104c009e
Miguel Mendez <miguel.mendez@novateva.com>
2022-02-27 23:20:29 -0400

    Orm Error Handler

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const routerApi = require("./routes")
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
@@ -27,6 +27,7 @@ app.get("/", (req, res)=> {
 routerApi(app);
 
 app.use(logErrors);
+app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
