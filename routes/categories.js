7b9b178717f30c6f805cefec1dd657d171b64bc0
Miguel <miguel.mendez@novateva.com>
2022-04-14 00:40:38 -0400

    Example router protected

--- routes/categories.js
+++ routes/categories.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 
 const CategoryService = require('./../services/categories');
 const validatorHandler = require('./../middlewares/validator.handler');
@@ -40,6 +41,7 @@ router.get('/:id',
 );
 
 router.post('/',
+  passport.authenticate('jwt', {session: false}),
   validatorHandler(createCategorySchema, 'body'),
   async (req, res, next) => {
     try {
